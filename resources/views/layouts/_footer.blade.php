852fdb2b3f4c7ac12e2e6d991e93cd1cfda47b60
vagrant <vagrant@homestead>
2019-12-29 10:20:53 +0000

    base layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right">laravel 学习参考</p>
+  </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+	<title>@yield('title', 'Laravel Shop') shop</title>
+	<link rel="stylesheet" type="text/css" href="{{mix('css/app.css')}}">
+</head>
+<body>
+	<div id="app" class="{{ route_class() }}-page">
+		@include('layouts._header')
+		<div class="container">
+			@yield('content')
+		</div>
+		@include('layouts._footer')
+	</div>
+	<script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
