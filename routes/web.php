3a5712a494f5e5732fb8fdba6bf33002e23ef800
vagrant <vagrant@homestead>
2020-01-10 03:59:22 +0000

    cart

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,24 @@ Auth::routes(['verify' => true]);
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function(){
 	Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+
+	Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+
+	Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
+
+	Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
+
+	Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
+
+	Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+
+	Route::post('cart', 'CartController@add')->name('cart.add');
+
+	Route::get('cart', 'CartController@index')->name('cart.index');
+
+	Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 });
+
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
